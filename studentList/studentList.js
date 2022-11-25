abd877b93a413c0fa0da9b5d177ba6de865ed853
Dmitry <iww511@mail.ru>
2022-11-25 03:33:08 +0300

    Commit at 3:31 am

--- studentList/studentList.js
+++ studentList/studentList.js
@@ -1,59 +1,59 @@
 
 const studentForm = document.getElementById('add-student__content')
 console.log(studentForm);
-class Student{
-
-    constructor(id, surname, name, patronymic, direction, duration) {
-        this.id = id;
-        this.surname = surname;
-        this.name = name;
-        this.patronymic = patronymic;
-        this.direction = direction;
-        this.duration = duration;
-        // this.includeStudentIntoPage()
-    }
-
-    getStudentAsElement() {
-        let className = "student" + this.id;
-        return `
-                 <section class="${className}" style="
-                     display: grid; 
-                     grid-template-columns: 7% 17% 16% 22% 19% 18%; 
-                     grid-column-gap: 0.2%;">
-                    <div>${this.id}</div>
-                    <div>${this.surname}</div>
-                    <div>${this.name}</div>
-                    <div>${this.patronymic}</div>
-                    <div>${this.direction}</div>
-                    <div>${this.duration}</div>
-                </section>
-               `
-    }
-
-    includeStudentIntoPage() {
-        const contentPage = document.getElementById("student-list__table-content");
-        contentPage.insertAdjacentHTML("beforeend", this.getStudentAsElement());
-    }
-
-    includeCharacteristicIntoPage() {
-
-    }
-
-}
-
-let studentList = [];
-
-studentForm.onsubmit = function(event) {
-    event.preventDefault();
-    let surname = studentForm.querySelector('[name="studentSurname"]').value;
-    let name = studentForm.querySelector('[name="studentName"]').value;
-    let patronymic = studentForm.querySelector('[name="studentPatronymic"]').value;
-    let direction = studentForm.querySelector('[name="direction"]').value;
-    let duration = studentForm.querySelector('[name="duration"]').value;
-    let arrLength = studentList.length;
-    window.location.href='../studentList/studentList.html';
-    let student = new Student(arrLength, surname, name, patronymic, direction, duration);
-    studentList.push(student);
-    console.log(studentList[0]);
-    student.includeStudentIntoPage();
-}
+// class Student{
+//
+//     constructor(id, surname, name, patronymic, direction, duration) {
+//         this.id = id;
+//         this.surname = surname;
+//         this.name = name;
+//         this.patronymic = patronymic;
+//         this.direction = direction;
+//         this.duration = duration;
+//         // this.includeStudentIntoPage()
+//     }
+//
+//     getStudentAsElement() {
+//         let className = "student" + this.id;
+//         return `
+//                  <section class="${className}" style="
+//                      display: grid;
+//                      grid-template-columns: 7% 17% 16% 22% 19% 18%;
+//                      grid-column-gap: 0.2%;">
+//                     <div>${this.id}</div>
+//                     <div>${this.surname}</div>
+//                     <div>${this.name}</div>
+//                     <div>${this.patronymic}</div>
+//                     <div>${this.direction}</div>
+//                     <div>${this.duration}</div>
+//                 </section>
+//                `
+//     }
+//
+//     includeStudentIntoPage() {
+//         const contentPage = document.getElementById("student-list__table-content");
+//         contentPage.insertAdjacentHTML("beforeend", this.getStudentAsElement());
+//     }
+//
+//     includeCharacteristicIntoPage() {
+//
+//     }
+//
+// }
+//
+// let studentList = [];
+//
+// studentForm.onsubmit = function(event) {
+//     event.preventDefault();
+//     let surname = studentForm.querySelector('[name="studentSurname"]').value;
+//     let name = studentForm.querySelector('[name="studentName"]').value;
+//     let patronymic = studentForm.querySelector('[name="studentPatronymic"]').value;
+//     let direction = studentForm.querySelector('[name="direction"]').value;
+//     let duration = studentForm.querySelector('[name="duration"]').value;
+//     let arrLength = studentList.length;
+//     window.location.href='../studentList/studentList.html';
+//     let student = new Student(arrLength, surname, name, patronymic, direction, duration);
+//     studentList.push(student);
+//     console.log(studentList[0]);
+//     student.includeStudentIntoPage();
+// }
