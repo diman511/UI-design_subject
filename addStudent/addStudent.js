feb140426e15c8407497c5ba6e40ea8c770894b8
Dmitry <iww511@mail.ru>
2022-12-05 00:46:12 +0300

    remove logic.js

--- addStudent/addStudent.js
+++ addStudent/addStudent.js
@@ -1,1 +1,18 @@
 
+const studentForm = document.getElementById('add-student__content')
+studentForm.onsubmit = function(event) {
+    event.preventDefault();
+    let studentDB = window.indexedDB.open("StudentDB", 1);
+    let surname = studentForm.querySelector('[name="studentSurname"]').value;
+    let name = studentForm.querySelector('[name="studentName"]').value;
+    let patronymic = studentForm.querySelector('[name="studentPatronymic"]').value;
+    let direction = studentForm.querySelector('[name="direction"]').value;
+    let duration = studentForm.querySelector('[name="duration"]').value;
+    studentDB.onerror = function(event) {
+        alert("DataBase error on code: " + event.target.errorCode);
+    };
+    studentDB.onsuccess = function(event) {
+        let dataBase = event.target.result;
+    };
+    window.location.href='../studentList/studentList.html';
+}
