a8de4d521a4334279660fb997ba6b3998ab11e83
Dmitry <iww511@mail.ru>
2022-12-05 23:17:14 +0300

    Try add indexedDB

--- addStudent/addStudent.js
+++ addStudent/addStudent.js
@@ -2,17 +2,50 @@
 const studentForm = document.getElementById('add-student__content')
 studentForm.onsubmit = function(event) {
     event.preventDefault();
-    let studentDB = window.indexedDB.open("StudentDB", 1);
+    let db;
+    let studentDB = window.indexedDB.open("StudentDB", 2);
     let surname = studentForm.querySelector('[name="studentSurname"]').value;
     let name = studentForm.querySelector('[name="studentName"]').value;
     let patronymic = studentForm.querySelector('[name="studentPatronymic"]').value;
     let direction = studentForm.querySelector('[name="direction"]').value;
     let duration = studentForm.querySelector('[name="duration"]').value;
+    let student = {
+        surname: surname,
+        name: name,
+        patronymic: patronymic,
+        direction: direction,
+        duration: duration
+    };
     studentDB.onerror = function(event) {
+        window.location.href='../studentList/studentList.html';
         alert("DataBase error on code: " + event.target.errorCode);
     };
     studentDB.onsuccess = function(event) {
-        let dataBase = event.target.result;
+        console.log('Open db --- onsuccess');
+        db = event.target.result;
+        if(!db.objectStoreNames.contains("students")) {
+            db.createObjectStore("students", {keyPath: "id", autoIncrement: true});
+        }
+        let transaction = db.transaction('students', 'readwrite');
+        let adding = transaction.objectStore('students');
+
+        let request = adding.add(student);
+        console.log(request);
+
+        request.onsuccess = function() {
+            console.log('Запись успешна');
+        }
+        request.onerror = function(event) {
+            console.log('Какаято ашибка', event.target.error);
+        }
+    };
+    studentDB.onupgradeneeded = function(event) {
+        console.log('Open db --- onupgradeneeded');
+        db = event.target.result;
+
+        if(!db.objectStoreNames.contains("students")) {
+            db.createObjectStore("students", {keyPath: "id", autoIncrement: true});
+        }
     };
     window.location.href='../studentList/studentList.html';
 }
--- studentList/studentList.js
+++ studentList/studentList.js
@@ -8,7 +8,6 @@ class Student{
         this.patronymic = patronymic;
         this.direction = direction;
         this.duration = duration;
-        // this.includeStudentIntoPage()
     }
 
     getStudentAsElement() {
@@ -17,13 +16,15 @@ class Student{
                  <section class="${className}" style="
                      display: grid;
                      grid-template-columns: 7% 17% 16% 22% 19% 18%;
-                     grid-column-gap: 0.2%;">
-                    <div>${this.id}</div>
-                    <div>${this.surname}</div>
-                    <div>${this.name}</div>
-                    <div>${this.patronymic}</div>
-                    <div>${this.direction}</div>
-                    <div>${this.duration}</div>
+                     grid-column-gap: 0.2%;
+                     font-size: 20px;
+                     height: 5vh;">
+                    <div id="field">${this.id}</div>
+                    <div id="field">${this.surname}</div>
+                    <div id="field">${this.name}</div>
+                    <div id="field">${this.patronymic}</div>
+                    <div id="field">${this.direction}</div>
+                    <div id="field">${this.duration}</div>
                 </section>
                `
     }
@@ -33,16 +34,44 @@ class Student{
         contentPage.insertAdjacentHTML("beforeend", this.getStudentAsElement());
     }
 
+    changeBackground(i){
+        console.log('hi')
+        let select = '.student' + (i+1);
+        console.log(select);
+        let nodes = document.querySelector(select).childNodes;
+        console.log(nodes);
+        for (let j = 1; j<nodes.length; j+=2){
+            nodes[j].style.background = 'indigo';
+        }
+        // this.name.style.background = 'indigo';
+    }
     includeCharacteristicIntoPage() {
 
     }
 
 }
 
-let studentList = [];
-// for (let i=0; i<numStudent; i++) {
-//     let student = new Student(arrLength, surname, name, patronymic, direction, duration);
-//     studentList.push(student);
-//     student.includeStudentIntoPage();
-// }
+let openRequest = indexedDB.open("StudentDB", 2);
+const studentList = [];
+
+openRequest.onerror = function (event) {
+    console.log("Error on open DB ", event.target.errorCode);
+
+};
+openRequest.onsuccess = function(event) {
+    let db = event.target.result;
+    let store = db.transaction('students', 'readonly').objectStore('students');
+
+    store.getAll().onsuccess = function (event) {
+        let studentArr = event.target.result;
+        for (let i=0; i<studentArr.length; i++) {
+            let student = new Student(studentArr[i].id, studentArr[i].surname, studentArr[i].name, studentArr[i].patronymic, studentArr[i].direction, studentArr[i].duration);
+            studentList.push(student);
+            student.includeStudentIntoPage();
+            if (i%2 == 0){
+                student.changeBackground(i);
+            }
+        }
+    }
+};
 
